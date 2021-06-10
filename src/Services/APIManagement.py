f7b4d1a11ee268ab2bce7c390d0f73814714d10b
PauGuiu <34050433+PauGuiu@users.noreply.github.com>
2021-06-09 22:07:34 -0500

    Update APIManagement.py

--- src/Services/APIManagement.py
+++ src/Services/APIManagement.py
@@ -1,5 +1,7 @@
 import requests
 
+#Class designed to comunicate the app 
+#with a API and Get JSON information.
 class APIManagement:
 
 	### Variables ###
@@ -20,6 +22,10 @@ class APIManagement:
     	print('URL: ' + self.url)
 
     ### Methods ###
+
+    #GetInfo: Get JSON Information from the API.
+    #Input: Void
+    #Output: JSON Object    
     def GetInfo(self):
         r = requests.get(self.url)
         return r.json()
