313f07e4a3dcfbef7fe8a14b806702e74a161f37
PauGuiu <34050433+PauGuiu@users.noreply.github.com>
2021-06-09 19:46:19 -0500

    Update ApiService.py

--- src/Services/ApiService.py
+++ src/Services/ApiService.py
@@ -1,11 +1,39 @@
 from sys import stderr
+from .APIManagement import APIManagement
+from .FileHandle import FileHandle
 
 
 class ApiService:
+
     def __init__(self):
         pass
 
     def run(self):
-        print('Running ApiService', file=stderr)
+        print('Running ApiService\n', file=stderr)
 
-        # TODO: follow README.md instructions
+        # TODO: follow README.md instructions        
+        #API URL
+        url = 'https://jsonplaceholder.typicode.com/todos/'
+
+        try:
+            # Get Info
+            print('Get TODOS from the API...')
+            API = APIManagement(url)        
+            info = API.GetInfo()        
+            print('Success\n')
+        
+            # Extracr Info and Generate File
+            print('Extracting data and generating files...\n')
+            for jsonElement in info:            
+                handle = FileHandle(jsonElement)
+                handle.JSONElementtoCSVFile()
+                print('File Generated succesfully: ' + handle.GetFileName())
+
+            #Finished
+            print('\nApiService Finished.')
+        
+        except IOError:
+                print("An I/O exception occurred while creating a file.")
+        except:
+                print("Some exception occurred.")      
+       
